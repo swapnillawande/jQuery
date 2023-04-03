8ccf2bca6b5030e102bc3fb4a07774c3693e0073
swapnil <swapnillawande2001@gmail.com>
2023-04-03 22:28:09 +0530

    v2

--- js/script.js
+++ js/script.js
@@ -7,17 +7,33 @@ $(function() {
   // $(".red-box").fadeIn(2000);
   // $(".red-box").fadeTo(1000,0.5);
 
-  $(".red-box").fadeTo(3000,0.2);
-  $(".green-box").fadeTo(2000,0.5);
-  $(".blue-box").fadeTo(1000,0.8);
+  // $(".red-box").fadeTo(3000,0.2);
+  // $(".green-box").fadeTo(2000,0.5);
+  // $(".blue-box").fadeTo(1000,0.8);
 
 
-  $(".blue-box").fadeToggle(2000);
-  $(".blue-box").fadeToggle(2000);
+  // $(".blue-box").fadeToggle(2000);
+  // $(".blue-box").fadeToggle(2000);
 
-  for(let i=0;i<10;i++){
-    $(".blue-box").fadeToggle(2000);
-  }
+  // for(let i=0;i<10;i++){
+  //   $(".blue-box").fadeToggle(2000);
+  // }
+
+
+  //$(".blue-box").hide(1000);
+  //$(".blue-box").show();
+
+  // $(".blue-box").fadeOut(2000);
+  // $(".blue-box1").fadeOut(2000);
+  // $(".blue-box").slideDown(2000);
+  // $(".blue-box1").slideDown(2000);
+
+  // for(let i=0;i<4;i++){
+
+  // }
+
+  $("p").hide(1000);
+  $("p").slideDown(1000);
 
 
 });
